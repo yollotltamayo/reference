e20c76acd3498679bfb8ef79b40ee06f6ccf7131
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-10-11 21:49:20 -0500

    corrige login

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,12 @@
-import Head from 'next/head'
-
 import { useRouter } from 'next/router'
 import {useEffect } from 'react'
-import useSWR from 'swr'
 
 function Home() {
 
-    const URI = process.env.URI || "";
     const r = useRouter()
 
     useEffect ( ()=> {
-        var  info ;
-        (async function ()  {
-            info = await  fetch(URI + '/user')
-            let res = await  info.json()
-            if(res.status && res.status === 'accepted') {
-                r.push('/board')
-            }else {
-                r.push('/login')
-            }
-        })();
+        r.push('/login')
     },[]);
 
     return (
